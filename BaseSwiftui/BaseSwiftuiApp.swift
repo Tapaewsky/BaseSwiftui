153c0e185669c33bfd52e08d87e380ea3260a15f
Said Tapaev <120788741+Tapaewsky@users.noreply.github.com>
2024-07-08 19:52:57 +0300

    Initial Commit

---
+++ BaseSwiftui/BaseSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseSwiftuiApp.swift
+//  BaseSwiftui
+//
+//  Created by Said Tapaev on 09.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BaseSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
